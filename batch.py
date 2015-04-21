6ca3f38281e1bdffe61d1e13dd6f571076ba3740
xingxux <vess527@gmail.com>
2015-04-21 00:12:16 -0700

    a

--- batch.py
+++ batch.py
@@ -2,7 +2,7 @@ import os
 
 enb = open("enbs.out").readlines()
 
-for xx in enb[:10]:
+for xx in enb[1:20]:
 	print xx
 	x = xx[:-1]
 	print x
@@ -10,4 +10,3 @@ for xx in enb[:10]:
 	os.system("python data_aggre.py LTE_BA 11 24-30 DL_BYTES ENODEB=%s,SERVICE_CATEGORY_ID=30"%(x))
 	os.system("python data_aggre.py LTE_BA 11 24-30 DL_BYTES ENODEB=%s,SERVICE_CATEGORY_ID=29"%(x))
 	os.system("python data_aggre.py LTE_BA 11 24-30 DL_BYTES ENODEB=%s"%(x))
-	break
